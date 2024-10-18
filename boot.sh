e93c86a35d4f321cc403afed3ec767e9a5939818
Joe Duncan <joe@n4jw.com>
2024-10-17 20:56:59 -0500

    init

--- boot.sh
+++ boot.sh
@@ -6,22 +6,24 @@ ascii_art='________                  __        ___.
 /    |    \  Y Y  \/ __ \|    <|  |  / \_\ \
 \_______  /__|_|  (____  /__|_ \____/|___  /
         \/      \/     \/     \/         \/
+                 For Pop!_OS
 '
 
 echo -e "$ascii_art"
-echo "=> Omakub is for fresh Ubuntu 24.04 installations only!"
+echo "Forked from Basecamp's Omakub"
+echo "=> Omakub is for fresh Pop!_OS 22.04 installations only!"
 echo -e "\nBegin installation (or abort with ctrl+c)..."
 
 sudo apt-get update >/dev/null
 sudo apt-get install -y git >/dev/null
 
-echo "Cloning Omakub..."
+echo "Cloning Omakub-pop_os..."
 rm -rf ~/.local/share/omakub
-git clone https://github.com/basecamp/omakub.git ~/.local/share/omakub >/dev/null
+git clone https://github.com/N4JW/omakub-pop_os.git ~/.local/share/omakub >/dev/null
 if [[ $OMAKUB_REF != "master" ]]; then
-	cd ~/.local/share/omakub
-	git fetch origin "${OMAKUB_REF:-stable}" && git checkout "${OMAKUB_REF:-stable}"
-	cd -
+  cd ~/.local/share/omakub
+  git fetch origin "${OMAKUB_REF:-stable}" && git checkout "${OMAKUB_REF:-stable}"
+  cd -
 fi
 
 echo "Installation starting..."
--- install/desktop/set-app-grid.sh
+++ install/desktop/set-app-grid.sh
@@ -8,15 +8,15 @@ sudo rm -rf /usr/share/applications/org.flameshot.Flameshot.desktop
 sudo rm -rf /usr/share/applications/display-im6.q16.desktop
 
 # Replacing this with btop
-sudo rm -rf /usr/share/applications/org.gnome.SystemMonitor.desktop
+#sudo rm -rf /usr/share/applications/org.gnome.SystemMonitor.desktop
 
 # We added our own meant for Alacritty
 sudo rm -rf /usr/local/share/applications/nvim.desktop
 sudo rm -rf /usr/local/share/applications/vim.desktop
 
 # Create folders
-gsettings set org.gnome.desktop.app-folders folder-children "['Utilities', 'Sundry', 'YaST', 'Updates', 'Xtra']"
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Updates/ name 'Install & Update'
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Updates/ apps "['org.gnome.Software.desktop', 'software-properties-drivers.desktop', 'software-properties-gtk.desktop', 'update-manager.desktop', 'firmware-updater_firmware-updater.desktop', 'snap-store_snap-store.desktop']"
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Xtra/ name 'Xtra'
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Xtra/ apps "['gnome-language-selector.desktop', 'org.gnome.PowerStats.desktop', 'yelp.desktop']"
+#gsettings set org.gnome.desktop.app-folders folder-children "['Utilities', 'Sundry', 'YaST', 'Updates', 'Xtra']"
+#gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Updates/ name 'Install & Update'
+#gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Updates/ apps "['org.gnome.Software.desktop', 'software-properties-drivers.desktop', 'software-properties-gtk.desktop', 'update-manager.desktop', 'firmware-updater_firmware-updater.desktop', 'snap-store_snap-store.desktop']"
+#gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Xtra/ name 'Xtra'
+#gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Xtra/ apps "['gnome-language-selector.desktop', 'org.gnome.PowerStats.desktop', 'yelp.desktop']"
