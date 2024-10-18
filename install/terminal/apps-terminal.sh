151c3741acdb520383598dd3c43bbda31fd027f2
Joe Duncan <joe@n4jw.com>
2024-10-18 16:49:39 -0500

    add eza back to terminal

--- install/terminal/apps-terminal.sh
+++ install/terminal/apps-terminal.sh
@@ -1,2 +1,2 @@
 # removed eza
-sudo apt install -y fzf ripgrep bat zoxide plocate btop apache2-utils fd-find tldr
+sudo apt install -y fzf ripgrep bat eza zoxide plocate btop apache2-utils fd-find tldr
