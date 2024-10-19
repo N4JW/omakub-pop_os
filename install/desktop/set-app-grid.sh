5c86328482874c8eed01da429c98681224e4ffc6
Joe Duncan <joe@n4jw.com>
2024-10-18 21:15:39 -0500

    scale back settings

--- install/desktop/set-app-grid.sh
+++ install/desktop/set-app-grid.sh
@@ -2,7 +2,7 @@
 sudo rm -rf /usr/share/applications/btop.desktop
 
 # App doesn't do anything when started from the app grid
-sudo rm -rf /usr/share/applications/org.flameshot.Flameshot.desktop
+#sudo rm -rf /usr/share/applications/org.flameshot.Flameshot.desktop
 
 # Remove the ImageMagick icon
 sudo rm -rf /usr/share/applications/display-im6.q16.desktop
